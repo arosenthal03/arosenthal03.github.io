5e30fc6310f8baff56151192534c1a436bf05cd0
arosenthal03 <157431817+arosenthal03@users.noreply.github.com>
2024-02-20 17:28:25 -0500

    Update lab5.js

--- lab5.js
+++ lab5.js
@@ -1,1 +1,3 @@
-<button type="button" id="myButton">Click Me!</button>
+var age = prompt("How old are you?", 30);
+alert("You are " + age + " years old!");
+// Alerting that 'you are input years old!
