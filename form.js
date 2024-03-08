ea01131c2668768dc8ae8351585d1033f3919eb7
arosenthal03 <157431817+arosenthal03@users.noreply.github.com>
2024-03-08 14:04:36 -0500

    Update form.js

---
+++ form.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("myform");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); 
+        
+        const formData = new FormData(form);
+
+        const formObject = {};
+        formData.forEach((value, key) => {
+            formObject[key] = value;
+        });
+
+        localStorage.setItem("formData", JSON.stringify(formObject));
+
+        form.reset();
+    });
+});
