8d747376f1d6868080d8199031b0f7cb7232ff6f
arosenthal03 <157431817+arosenthal03@users.noreply.github.com>
2024-03-08 11:17:32 -0500

    Update form.js

--- form.js
+++ form.js
@@ -1,1 +1,14 @@
+// Function to reset the form
+function clearForm() {
+    document.getElementById('myform').reset();
+  }
   
+  document.addEventListener('DOMContentLoaded', function () {
+    const clearBtn = document.getElementById('clearBtn');
+  
+    // Add click event listener to clear/reset button
+    clearBtn.addEventListener('click', function () {
+      // Call the clearForm function to reset the form
+      clearForm();
+    });
+  });  
