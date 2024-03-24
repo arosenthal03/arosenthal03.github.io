13d201280493d3e06bc550f315f7dd2340e433fc
arosenthal03 <157431817+arosenthal03@users.noreply.github.com>
2024-03-24 18:13:49 -0400

    s

--- form.js
+++ form.js
@@ -30,3 +30,19 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }   
 });
+
+function setNormal() {
+    document.body.className = '';
+}
+        
+function setProtanopia() {
+    document.body.className = 'protanopia';
+}
+        
+function setDeuteranopia() {
+    document.body.className = 'deuteranopia';
+}
+        
+function setTritanopia() {
+    document.body.className = 'tritanopia';
+}
