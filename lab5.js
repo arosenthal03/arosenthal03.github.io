633654d9eede0e69275a5fe2362c3262377b06c5
arosenthal03 <157431817+arosenthal03@users.noreply.github.com>
2024-03-24 17:44:19 -0400

    s

--- lab5.js
+++ lab5.js
@@ -19,19 +19,5 @@ document.addEventListener("DOMContentLoaded", function()
         {handleMouseOver(this);});
         navItems[i].addEventListener("mouseout", function() 
         {handleMouseOut(this);});}});
-        function changeFontSize(action) {
-            var body = document.querySelector('body');
-            if (action === 'increase') {
-                body.classList.add('large-font');
-            } else if (action === 'decrease') {
-                body.classList.remove('large-font');
-            }
-        }
-        function changeColorScheme(scheme) {
-            var body = document.querySelector('body');
-            if (scheme === 'default') {
-                body.classList.remove('high-contrast');
-            } else if (scheme === 'high-contrast') {
-                body.classList.add('high-contrast');
-            }
-        }
+
+        
