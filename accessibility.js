cc7a05d4129a775f1de7641c1e4dcd1a8e7227b8
arosenthal03 <157431817+arosenthal03@users.noreply.github.com>
2024-03-24 17:47:31 -0400

    s

--- accessibility.js
+++ accessibility.js
@@ -1,16 +1,15 @@
-function changeFontSize(action) {
-    var body = document.querySelector('body');
-    if (action === 'increase') {
-        body.classList.add('large-font');
-    } else if (action === 'decrease') {
-        body.classList.remove('large-font');
+function setNormal() {
+    document.body.className = '';
     }
-}
-function changeColorScheme(scheme) {
-    var body = document.querySelector('body');
-    if (scheme === 'default') {
-        body.classList.remove('high-contrast');
-    } else if (scheme === 'high-contrast') {
-        body.classList.add('high-contrast');
+        
+function setProtanopia() {
+    document.body.className = 'protanopia';
     }
-}
+        
+function setDeuteranopia() {
+    document.body.className = 'deuteranopia';
+    }
+        
+function setTritanopia() {
+    document.body.className = 'tritanopia';
+    }
